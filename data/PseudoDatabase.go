a5dbab0ce38d4bd9f4543024ec44c684a17fd3ff
Renato Chitolina <renato.chitolina@immutable.com>
2022-03-15 17:58:02 -0300

    Complementing domain entity to fit the domain needs. Creating a pseudo database that will contain all setup needed to check and apply business rules on data that come from providers (sensors)

---
+++ data/PseudoDatabase.go
@@ -0,0 +1,45 @@
+package data
+
+import "server/models"
+
+func SensorSetups() map[string]models.Sensor {
+	var data = map[string]models.Sensor{
+		"1": {
+			Id:                 "1",
+			Name:               "Pilsner",
+			MinimumTemperature: 4,
+			MaximumTemperature: 6,
+		},
+		"2": {
+			Id:                 "2",
+			Name:               "IPA",
+			MinimumTemperature: 5,
+			MaximumTemperature: 6,
+		},
+		"3": {
+			Id:                 "3",
+			Name:               "Lager",
+			MinimumTemperature: 4,
+			MaximumTemperature: 7,
+		},
+		"4": {
+			Id:                 "4",
+			Name:               "Stout",
+			MinimumTemperature: 6,
+			MaximumTemperature: 8,
+		},
+		"5": {
+			Id:                 "5",
+			Name:               "Wheat beer",
+			MinimumTemperature: 3,
+			MaximumTemperature: 5,
+		},
+		"6": {
+			Id:                 "6",
+			Name:               "Pale Ale",
+			MinimumTemperature: 4,
+			MaximumTemperature: 6,
+		}}
+
+	return data
+}
--- models/Sensor.go
+++ models/Sensor.go
@@ -1,6 +1,9 @@
 package models
 
 type Sensor struct {
-	Id          string `json:"id"`
-	Temperature int8   `json:"temperature"`
+	Id                 string `json:"id"`
+	Name               string
+	MinimumTemperature int8
+	MaximumTemperature int8
+	Temperature        int8 `json:"temperature"`
 }
